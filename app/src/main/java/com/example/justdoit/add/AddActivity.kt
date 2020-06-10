e95a16890c388ec59ef12dc6b7d96e35162e9395
MinaN <minadevdream@gmail.com>
2020-06-10 21:53:28 +0200

    create two main activities

--- app/src/main/java/com/example/justdoit/add/AddActivity.kt
+++ app/src/main/java/com/example/justdoit/add/AddActivity.kt
@@ -1,9 +1,10 @@
-package com.example.justdoit
+package com.example.justdoit.add
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.justdoit.R
 
-class MainActivity : AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/justdoit/list/ListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.justdoit.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.justdoit.R
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
