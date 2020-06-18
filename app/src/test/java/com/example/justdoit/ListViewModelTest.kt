e261af562ef4e5f8d8d180903c86052517f06bd2
MinaN <minadevdream@gmail.com>
2020-06-18 21:51:50 +0200

    test: add mocking for a todo repo in List view model test

--- app/src/test/java/com/example/justdoit/ListViewModelTest.kt
+++ app/src/test/java/com/example/justdoit/ListViewModelTest.kt
@@ -5,41 +5,25 @@ import com.example.justdoit.data.ToDo
 import com.example.justdoit.data.ToDoRepository
 import com.example.justdoit.list.ListViewModel
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
+import java.lang.IllegalArgumentException
 
 class ListViewModelTest {
-
+    //unit testing prioritize readability over reuse
+    // so it is okay to use the same variable across multiple classes
+    val now = System.currentTimeMillis()
+    val day = 1000 * 60 * 60 * 24
     //this tells junit that we are not expecting exceptions on tests
     //what gives us the ability to capture them on a per test basis
     //since kotlin makes all properties private by default we will need to add agetter
     @get:Rule
     val exceptionRule: ExpectedException = ExpectedException.none()
-    @Before
-    fun setup(){
-        //unit testing prioritize readability over reuse
-        // so it is okay to use the same variable across multiple classes
-        val now = System.currentTimeMillis()
-        val day = 1000 * 60 * 60 * 24
-
-        val todos = ArrayList<ToDo>()
-        var toDo = ToDo(1,"ToDo 1",null,false,now)
-        todos.add(toDo)
-        toDo = ToDo(2,"ToDo 2",now + day,false,now)
-        todos.add(toDo)
-        toDo = ToDo(3,"ToDo 3",now + day,true,now)
-        todos.add(toDo)
-        toDo = ToDo(4,"ToDo 4",now + day,false,now)
-        todos.add(toDo)
-        toDo = ToDo(5,"ToDo 5",now + day,false,now)
-        todos.add(toDo)
-
-
-    }
 
     @Test
     fun `getAllTodos() with an empty todos then return an empty arraylist()`(){
@@ -47,6 +31,39 @@ class ListViewModelTest {
         val repository : ToDoRepository = mock()
         whenever(repository.getAllToDos())
             .thenReturn(MutableLiveData(arrayListOf()))
+    val model = ListViewModel(repository)
+
+        val todos = model.allTodos.value
+        assertNotNull(todos)
+        assertEquals(expectedCount,todos!!.size)
+
+    }
+
+    @Test
+    fun `getAllTodos() with an single todo then return an arraylist contains single todo()`(){
+        val expectedCount = 1
+        val repository : ToDoRepository = mock()
+        whenever(repository.getAllToDos())
+            .thenReturn(MutableLiveData(arrayListOf(
+                ToDo(5,"ToDo 5",now + day,false,now)
+            )))
+        val model = ListViewModel(repository)
+
+        val todos = model.allTodos.value
+        assertNotNull(todos)
+        assertEquals(expectedCount,todos!!.size)
+
+    }
+    @Test
+    fun `getAllTodos() with an multiple todos then return an arraylist contains the same number of todos()`(){
+        val expectedCount = 3
+        val repository : ToDoRepository = mock()
+        whenever(repository.getAllToDos())
+            .thenReturn(MutableLiveData(arrayListOf(
+                ToDo(5,"ToDo 5",now + day,false,now),
+                ToDo(6,"ToDo 6",now - day,false,now),
+                ToDo(7,"ToDo 7",now + day,false,now)
+            )))
         val model = ListViewModel(repository)
 
         val todos = model.allTodos.value
@@ -55,22 +72,68 @@ class ListViewModelTest {
 
     }
 
-//    @Test
-//    fun `getUpcomingTodosCount with 3 upcoming todos then return 3`(){
-//        val expectedCount = 3
-//        val model = ListViewModel(repository)
-//
-//        val todosCount = model.upcomingTodosCount.value
-//        assertNotNull(todosCount)
-//        assertEquals(expectedCount,todosCount)
-//    }
-//
-//    @Test
-//    fun test_toggleTodo(){
-//        val id = -1L
-//        val model = ListViewModel(repository)
-//
-//        exceptionRule.expect(NotImplementedError::class.java)
-//        model.toggleTodo(id)
-//    }
+    @Test
+    fun `getUpcomingTodosCount with 0 upcoming todos then return 0`(){
+        val repository : ToDoRepository = mock()
+        val expectedCount = 0
+        whenever(repository.getUpcomingToDosCount())
+            .thenReturn(MutableLiveData(expectedCount))
+        val model = ListViewModel(repository)
+
+        val todosCount = model.upcomingTodosCount.value
+        assertNotNull(todosCount)
+        assertEquals(expectedCount,todosCount)
+    }
+     @Test
+    fun `getUpcomingTodosCount with 1 upcoming todos then return 1`(){
+        val repository : ToDoRepository = mock()
+        val expectedCount = 1
+        whenever(repository.getUpcomingToDosCount())
+            .thenReturn(MutableLiveData(expectedCount))
+        val model = ListViewModel(repository)
+
+        val todosCount = model.upcomingTodosCount.value
+        assertNotNull(todosCount)
+        assertEquals(expectedCount,todosCount)
+    }
+     @Test
+    fun `getUpcomingTodosCount with 5 upcoming todos then return 5`(){
+        val repository : ToDoRepository = mock()
+        val expectedCount = 5
+        whenever(repository.getUpcomingToDosCount())
+            .thenReturn(MutableLiveData(expectedCount))
+        val model = ListViewModel(repository)
+
+        val todosCount = model.upcomingTodosCount.value
+        assertNotNull(todosCount)
+        assertEquals(expectedCount,todosCount)
+    }
+
+
+    @Test
+    fun `test_toggleTodo with a present todo then verify behavior of toggletodo method`(){
+        val repository : ToDoRepository = mock()
+        val id = -1L
+        val model = ListViewModel(repository)
+
+        model.toggleTodo(id)
+
+        verify(repository)
+            .toggleToDo(id)
+    }
+    @Test
+    fun `test_toggleTodo with no todo found then throw illegalArgumend exception` (){
+        val repository : ToDoRepository = mock()
+        val exceptionMessage = "Todo not found"
+        val id = -1L
+        whenever(repository.toggleToDo(id))
+            .thenThrow(IllegalArgumentException(exceptionMessage))
+        val model = ListViewModel(repository)
+        exceptionRule.expect(IllegalArgumentException::class.java)
+        exceptionRule.expectMessage(exceptionMessage)
+        model.toggleTodo(id)
+
+        verify(repository)
+            .toggleToDo(id)
+    }
 }
